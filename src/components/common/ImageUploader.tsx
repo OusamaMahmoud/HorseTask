07402229751da24ff82886681b9af9b9853b92e6
Osama Mahmoud <ousama.mah113@gmail.com>
2025-04-12 02:28:21 +0200

    Remove unused Imports.

--- src/components/common/ImageUploader.tsx
+++ src/components/common/ImageUploader.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import axios from "axios";
 import { useMutation } from "@tanstack/react-query";
 import apiClient from "../../services/axios";
 
--- src/components/common/Modal.tsx
+++ src/components/common/Modal.tsx
@@ -1,5 +1,3 @@
-import React, { useState } from 'react';
-
 interface ModalProps {
   isOpen: boolean;
   onClose: () => void;
